fbfb54b2c1dac12aa6ce5e05c798ef5ef46739b4
Kerry <kdrussell.jr@gmail.com>
2019-05-28 11:59:04 -0500

    added function

--- turtleDrawing.py
+++ turtleDrawing.py
@@ -1,10 +1,18 @@
 import turtle
 
 my_turtle = turtle.Turtle()
+
+#this draws the square
+def square():
+    my_turtle.forward(100)
+    my_turtle.left(90)
+    my_turtle.forward(100)
+    my_turtle.left(90)
+    my_turtle.forward(100)
+    my_turtle.left(90)
+    my_turtle.forward(100)
+
+square()
 my_turtle.forward(100)
-my_turtle.left(90)
-my_turtle.forward(100)
-my_turtle.left(90)
-my_turtle.forward(100)
-my_turtle.left(90)
-my_turtle.forward(100)
+square()
+
