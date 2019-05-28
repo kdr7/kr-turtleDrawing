24d099fccfea045aa3e5f560e794841978e92ff1
Kerry <kdrussell.jr@gmail.com>
2019-05-28 12:01:32 -0500

    added comments

--- turtleDrawing.py
+++ turtleDrawing.py
@@ -1,9 +1,12 @@
+#calls turtle library
 import turtle
 
+#creates variable my_turtle
 my_turtle = turtle.Turtle()
 
-#this draws the square
+#creates a function
 def square():
+    #draws the square
     my_turtle.forward(100)
     my_turtle.left(90)
     my_turtle.forward(100)
@@ -12,6 +15,7 @@ def square():
     my_turtle.left(90)
     my_turtle.forward(100)
 
+#calls function
 square()
 my_turtle.forward(100)
 square()
