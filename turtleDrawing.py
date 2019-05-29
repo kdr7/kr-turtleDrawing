f176bd6db9cf9ef1a3545ae6e04db249b6b92e68
Kerry <kdrussell.jr@gmail.com>
2019-05-29 10:46:00 -0500

    added loop

--- turtleDrawing.py
+++ turtleDrawing.py
@@ -4,19 +4,16 @@ import turtle
 #creates variable my_turtle
 my_turtle = turtle.Turtle()
 
-#creates a function
-def square():
+#creates a function with multiple arguments
+def square(length, angle):
     #draws the square
-    my_turtle.forward(100)
-    my_turtle.left(90)
-    my_turtle.forward(100)
-    my_turtle.left(90)
-    my_turtle.forward(100)
-    my_turtle.left(90)
-    my_turtle.forward(100)
+    my_turtle.forward(length)
+    my_turtle.left(angle)
+    my_turtle.forward(length)
+    my_turtle.left(angle)
+    my_turtle.forward(length)
+    my_turtle.left(angle)
+    my_turtle.forward(length)
 
-#calls function
-square()
-my_turtle.forward(100)
-square()
-
+for i in range(4):
+    square(100, 90)
